21a5df04fbb5066821fb156dc22ccc54343a6c4e
temitope femitemiola <temitope.femitemiola@md.catapult.org.uk>
2024-09-01 21:55:42 +0100

    created function to extract data from postgres

---
+++ scripts/export_db_to_azure.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS informatics;
+CREATE DATABASE informatics;
+\connect informatics
+CREATE SCHEMA coshh;
+
+CREATE OR REPLACE FUNCTION export_chemical_data(file_path TEXT)
+RETURNS VOID AS $$
+BEGIN
+    COPY (
+        SELECT
+            coshh.chemical.*,
+            coshh.chemical_to_hazard.*
+        FROM
+            coshh.chemical
+        JOIN
+            coshh.chemical_to_hazard ON coshh.chemical.id = coshh.chemical_to_hazard.id
+    ) TO file_path WITH CSV HEADER;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT export_chemical_data('/path/to/azure_data.csv');
